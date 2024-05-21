4f913226360987c5a62d5047f3779e684322134d
juan antonio <juanantonioros20@gmail.com>
2024-05-21 22:41:07 +0200

    prueba modal

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image, Modal, Button } from 'react-native';
+import { AntDesign } from '@expo/vector-icons'; // Asegúrate de tener instalada la biblioteca de iconos
+
 
 const LoginScreen = () => {
 
@@ -7,6 +9,8 @@ const LoginScreen = () => {
 
   return (
     <View style={styles.container}>
+          <View style={styles.containercenter}>
+
       <Image source={require('../assets/logo.png')} style={styles.logo} />
       <TextInput
         style={styles.input}
@@ -27,6 +31,8 @@ const LoginScreen = () => {
           <Text style={styles.buttonTextLogin}>Login</Text>
         </TouchableOpacity>
       </View>
+      </View>
+
 
       <Modal
         animationType="slide"
@@ -36,10 +42,45 @@ const LoginScreen = () => {
           setModalVisible(!modalVisible);
         }}
       >
-        <View style={styles.modalContainer}>
+         <View style={styles.modalContainer}>
           <View style={styles.modalContent}>
-            <Text style={styles.modalText}>Bienvenido!</Text>
-            <Button title="Cerrar"  onPress={() => setModalVisible(!modalVisible)}/>
+            <TouchableOpacity
+              style={styles.closeButton}
+              onPress={() => setModalVisible(!modalVisible)}
+            >
+              <AntDesign name="close" size={24} color="black" />
+            </TouchableOpacity>
+
+            <Text style={styles.label}>NOMBRE</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Enter your name"
+              placeholderTextColor="#666"
+            />
+
+            <Text style={styles.label}>CONTRASEÑA</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Enter your password..."
+              placeholderTextColor="#666"
+              secureTextEntry
+            />
+
+            <Text style={styles.label}>CORREO ELECTRÓNICO</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Enter your email"
+              placeholderTextColor="#666"
+            />
+
+            <View style={styles.buttonContainer}>
+              <TouchableOpacity style={styles.studentButton}>
+                <Text style={styles.buttonTextStudent}>ALUMNO</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.teacherButton}>
+                <Text style={styles.buttonTextTeacher}>PROFESOR</Text>
+              </TouchableOpacity>
+            </View>
           </View>
         </View>
       </Modal>
@@ -53,6 +94,14 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#fff',
+  },
+  containercenter:{
+    
+    width: '80%',
+   
+    alignItems: 'center',
+    position: 'relative',
+    
   },
   logo: {
     width: 250,
@@ -102,20 +151,70 @@ const styles = StyleSheet.create({
   },
   modalContainer: {
     flex: 1,
-    width: 300,   
+    width: '100%',   
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#ff    f',
+    backgroundColor: 'rgba(114, 114, 114, 0.95)',
   },
   modalContent: {
+    width: '80%',
     backgroundColor: '#fff',
     padding: 20,
     borderRadius: 10,
     alignItems: 'center',
+    position: 'relative',
   },
-  modalText: {
-    fontSize: 20,
-    marginBottom: 20,
+  closeButton: {
+    position: 'absolute',
+    top: 10,
+    right: 10,
+  },
+  label: {
+    alignSelf: 'flex-start',
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginTop: 10,
+  },
+  input: {
+    width: '100%',
+    height: 40,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginVertical: 10,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+    marginTop: 20,
+  },
+  studentButton: {
+    flex: 1,
+    backgroundColor: '#fff',
+    borderColor: '#000',
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingVertical: 10,
+    alignItems: 'center',
+    marginRight: 5,
+  },
+  teacherButton: {
+    flex: 1,
+    backgroundColor: '#000',
+    borderColor: '#000',
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingVertical: 10,
+    alignItems: 'center',
+    marginLeft: 5,
+  },
+  buttonTextStudent: {
+    color: '#000',
+  },
+  buttonTextTeacher: {
+    color: '#fff',
   },
 });
 

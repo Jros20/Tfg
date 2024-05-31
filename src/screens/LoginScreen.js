ae16b2a419207c5cbf8178b93b6fcffd0bbe9b3f
juan antonio <juanantonioros20@gmail.com>
2024-05-31 18:17:40 +0200

    inicio firebase login

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,28 +1,13 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image, Modal } from 'react-native';
-import { AntDesign } from '@expo/vector-icons'; // Asegúrate de tener instalada la biblioteca de iconos
-import Animated, { Easing, useSharedValue, useAnimatedStyle, withTiming } from 'react-native-reanimated';
-import { useNavigation } from '@react-navigation/native'; // Importamos useNavigation
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image } from 'react-native';
+import RegisterModal from '../components/RegisterModal'; // Ajusta la ruta según la ubicación de RegisterModal.js
 
 const LoginScreen = () => {
   const [modalVisible, setModalVisible] = useState(false);
-  const opacity = useSharedValue(0);
-  const navigation = useNavigation(); // Obtenemos la navegación
 
   const openModal = () => {
     setModalVisible(true);
-    opacity.value = withTiming(1, { duration: 500, easing: Easing.inOut(Easing.ease) });
   };
-
-  const closeModal = () => {
-    opacity.value = withTiming(0, { duration: 500, easing: Easing.inOut(Easing.ease) }, false); 
-    setModalVisible(false);
-  };
-
-  const animatedStyle = useAnimatedStyle(() => ({
-    opacity: opacity.value,
-  }));
-
 
   return (
     <View style={styles.container}>
@@ -40,7 +25,7 @@ const LoginScreen = () => {
           secureTextEntry
         />
         <View style={styles.buttonContainer}>
-          <TouchableOpacity style={styles.registerButton}  onPress={openModal}>
+          <TouchableOpacity style={styles.registerButton} onPress={openModal}>
             <Text style={styles.buttonTextRegister}>Register</Text>
           </TouchableOpacity>
           <TouchableOpacity style={styles.loginButton}>
@@ -49,58 +34,10 @@ const LoginScreen = () => {
         </View>
       </View>
 
-      <Modal
-        transparent={true}
-        visible={modalVisible}
-        onRequestClose={closeModal} 
-      >
-        <View style={styles.modalContainer}>
-          <Animated.View style={[styles.modalContent, animatedStyle]}>
-            <TouchableOpacity
-              style={styles.closeButton}
-              onPress={closeModal} 
-            >
-              <AntDesign name="close" size={24} color="black" />
-            </TouchableOpacity>
-
-            <Text style={styles.label}>NOMBRE</Text>
-            <TextInput
-              style={styles.modalInput}
-              placeholder="Enter your name"
-              placeholderTextColor="#666"
-            />
-
-            <Text style={styles.label}>CONTRASEÑA</Text>
-            <TextInput
-              style={styles.modalInput}
-              placeholder="Enter your password..."
-              placeholderTextColor="#666"
-              secureTextEntry
-            />
-
-            <Text style={styles.label}>CORREO ELECTRÓNICO</Text>
-            <TextInput
-              style={styles.modalInput}
-              placeholder="Enter your email"
-              placeholderTextColor="#666"
-            />
-
-            <View style={styles.buttonContainer}>
-              <TouchableOpacity style={styles.studentButton}  onPress={() => navigation.navigate('UserInterface')}>
-                <Text style={styles.buttonTextStudent}>ALUMNO</Text>
-              </TouchableOpacity>
-              <TouchableOpacity style={styles.teacherButton } onPress={() => navigation.navigate('TeacherInterface')}>
-                <Text style={styles.buttonTextTeacher}>PROFESOR</Text>
-              </TouchableOpacity>
-            </View>
-          </Animated.View>
-        </View>
-      </Modal>
+      <RegisterModal modalVisible={modalVisible} setModalVisible={setModalVisible} />
     </View>
   );
 };
-
-
 
 const styles = StyleSheet.create({
   container: {
@@ -158,79 +95,6 @@ const styles = StyleSheet.create({
   },
   buttonTextLogin: {
     color: '#fff',
-  },
-  modalContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    width:'90%',
-    marginStart:'5%',
-    height:'90%',
-    marginBottom:'20%'
-
-  },
-  modalContent: {
-    width: '80%',
-    backgroundColor: '#cccccc',
-    padding: 30,
-    borderRadius: 10,
-    alignItems: 'center',
-    position: 'relative',
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 4,
-    },
-    shadowOpacity: 0.3,
-    shadowRadius: 4,
-    elevation: 5,
-  },
-  closeButton: {
-    position: 'absolute',
-    top: 10,
-    right: 10,
-  },
-  label: {
-    alignSelf: 'flex-start',
-    fontSize: 16,
-    fontWeight: 'bold',
-    marginTop: 10,
-  },
-  modalInput: {
-    width: '100%',
-    height: 40,
-
-    borderColor: '#fff',
-    borderWidth: 1,
-    borderRadius: 5,
-    paddingHorizontal: 10,
-    marginVertical: 20,
-  },
-  buttonTextStudent: {
-    color: '#000',
-  },
-  buttonTextTeacher: {
-    color: '#fff',
-  },
-  studentButton: {
-    flex: 1,
-    backgroundColor: '#fff',
-    borderColor: '#000',
-    borderWidth: 1,
-    borderRadius: 5,
-    paddingVertical: 10,
-    alignItems: 'center',
-    marginRight: 5,
-  },
-  teacherButton: {
-    flex: 1,
-    backgroundColor: '#000',
-    borderColor: '#000',
-    borderWidth: 1,
-    borderRadius: 5,
-    paddingVertical: 10,
-    alignItems: 'center',
-    marginLeft: 5,
   },
 });
 
